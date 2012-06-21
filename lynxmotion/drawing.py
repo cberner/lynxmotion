4f010ad24f694ad80b2488e0f14534edca8ab4b8
Christopher Berner <christopherberner@gmail.com>
2012-06-20 22:00:25 -0700

    Prototype line drawing library. Doesn't quite draw straight lines, as our IK doesn't support the wrist yet

---
+++ lynxmotion/drawing.py
@@ -0,0 +1,37 @@
+import al5d
+import math
+
+class Drawing(object):
+    def __init__(self, serial_port='/dev/ttyUSB0'):
+        self.al5d = al5d.AL5D(serial_port=serial_port)
+
+    def init(self):
+        self.al5d.init()
+        self.al5d.move(0, 0.2, 0.05)
+        print "Please place the pen"
+
+    def grip_pen(self):
+        self.al5d.gripper(78)
+
+    def line(self, start_x, start_y, end_x, end_y):
+        self.al5d.move(0, 0.2, 0.2)
+        self.al5d.wait_for_move()
+
+        distance = math.sqrt((end_x - start_x)**2 + (end_y - start_y)**2)
+        #Take steps that are 0.5mm
+        num_steps = int(distance * 2000)
+        
+        #Parameterize x and y in terms of t
+        slope_x = (end_x - start_x) / num_steps
+        slope_y = (end_y - start_y) / num_steps
+        print 'taking {0} steps'.format(num_steps)
+        for t in range(num_steps):
+            self.al5d.move(start_x + t*slope_x, start_y + t*slope_y, 0.05)
+            self.al5d.wait_for_move()
+
+        #Make sure we end exactly where requested
+        #(num_steps is descretized so we might not)
+        self.al5d.move(end_x, end_y, 0.05)
+        self.al5d.wait_for_move()
+        self.al5d.move(0, 0.2, 0.2)
+        self.al5d.wait_for_move()
